48fec3aae8181d0b7380c6c62c1c5a26166566b3
Bhanupratap Rathore <f20201675@pilani.bits-pilani.ac.in>
2023-02-12 22:45:33 +0530

    lexor.c

--- lexer.c
+++ lexer.c
@@ -1,54 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define buff_size 50
+#define buff_size 500
 
-char buff1[buff_size];
-char buff2[buff_size];
+char buffer1[buff_size];
+char buffer2[buff_size];
 
-bool fill_1=false,fill_2=false;
+bool is_buffer1_filled = false, is_buffer2_filled = false;
+
+char *begin_ptr, *forward_ptr;
+
+FILE *fptr;
 
 typedef struct token
 {
-   char token_type[20];
-   char lexeme[30];
-   int line_no; 
-}Token;
+    char token_type[20];
+    char lexeme[30];
+    int line_no;
+} Token;
 
+void start_lexer(FILE *fp)
+{
+    begin_ptr = buffer1;
+    forward_ptr = begin_ptr;
 
-void start_lexer(FILE *fp){
-    fread(buff1,1,buff_size,fp);
+    is_buffer1_filled = true;
+
+    fread(buffer1, 1, buff_size, fp);
+
+    printf("%s\n", buffer1);
 }
 
-
-Token get_next_token(){
-int state=0;
-
-
+void fill_buffer()
+{
+    if (is_buffer1_filled)
+    {
+        fread(buffer2, 1, buff_size, fptr);
+        is_buffer1_filled = false;
+        is_buffer2_filled = true;
+    }
 }
 
+char get_next_char(char *forward_ptr)
+{
+    if (*forward_ptr == '\0')
+    {
+        fill_buffer();
+    }
+    else if (*forward_ptr == EOF)
+    {
+        // close_lexor();
+    }
+    return *(forward_ptr++);
+}
 
-int main(){
-FILE *fptr;
-fptr = fopen("./Testcases/Testcase1","r");
-if(fptr==NULL){
-    printf("File not opened\n");
+Token get_next_token()
+{
+    int current_state = 0;
+
+    while (1)
+    {
+        switch (current_state)
+        {
+        case 0:
+        {
+            char incoming = get_next_char(forward_ptr);
+            printf("%c\n", incoming);
+            break;
+        }
+
+        default:
+            break;
+        }
+    }
+
+    // return NULL;
+}
+
+int main()
+{
+    fptr = fopen("./Testcases/Testcase1", "r");
+    if (fptr == NULL)
+    {
+        printf("File not opened\n");
+        return 0;
+    }
+    start_lexer(fptr);
+    // char *begin,*forward;
+    // forward=buff1;
+    // int lineno=1;
+    // int i=0;
+    // printf("%s\n",buff1);
+    // for(i=0;i<buff_size;i++){
+    //     if(buff1[i]=='\n'){
+    //         lineno++;
+    //     }
+    // }
+    // printf("%d\n",lineno);
+    while (1)
+    {
+        Token current_token = get_next_token();
+    }
     return 0;
-}
-start_lexer(fptr);
-// char *begin,*forward;
-// forward=buff1;
-// int lineno=1;
-// int i=0;
-// printf("%s\n",buff1);
-// for(i=0;i<buff_size;i++){
-//     if(buff1[i]=='\n'){
-//         lineno++;
-//     }
-// }
-// printf("%d\n",lineno);
-while(1){
-    Token current_token=get_next_token();
-}
-return 0;
 }
---
+++ script.sh
@@ -0,0 +1,6 @@
+gcc -c lexer.c
+gcc -o a.out lexer.o
+./a.out
+
+rm *.o
+# rm *a.out
